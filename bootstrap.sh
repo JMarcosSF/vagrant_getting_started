0281add173959d655000278b533562d4a4d45f25
U-DSFW\jmarcos <jmarcos@GYKDJS1.ad.businesswire.com>
2015-04-02 13:05:05 -0700

    updated bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,13 +1,14 @@
 #!/usr/bin/env bash
+add-apt-repository ppa:webupd8team/java
+add-apt-repository ppa:cwchien/gradle
 apt-get update
-apt-get install -y apache2
 apt-get -y install tmux vim zsh git-core rabbitmq-server
+apt-get install -y apache2
 add-apt-repository ppa:webupd8team/java
 apt-get install -y oracle-java7-installer
 update-java-alternatives -s java-7-oracle
 apt-get install -y npm
 npm install -bower
-
 
 sudo apt-get install -y scala
 
